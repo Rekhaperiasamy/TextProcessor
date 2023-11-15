dc70a4d661d44250d8bf1a2fd2f8f517c245eb69
Rekhaperiasamy <rekha_p@outlook.com>
2023-11-15 16:06:49 +0100

    Added doc strings

--- src/WordFrequencyAnalyzer.py
+++ src/WordFrequencyAnalyzer.py
@@ -1,16 +1,27 @@
-from typing import List
+from typing import List, Dict, Tuple
 import re
 
 from src.WordFrequency import WordFrequency
 
 
 class WordFrequencyAnalyzer:
-    def __init__(self) -> None:
-        pass
+    '''
+    This class analyses the given text and performs word frequency calculations 
+    '''
 
     def calculate_highest_frequency(self, text: str) -> int:
-        word_list = re.findall(
-            r'(?<![A-Za-z])[A-Za-z]+(?![A-Za-z])', text.lower())
+        '''
+        This method calculates the highest word frequency 
+        and returns the count of the highest frequency from the given text
+
+        Args:
+            text (str): Text that needs to be analysed
+
+        Returns:
+            highest_frequency (int): count of the highest frequency
+        '''
+
+        word_list = self._constructWordList(text)
 
         if (not word_list):
             return 0
@@ -20,8 +31,18 @@ class WordFrequencyAnalyzer:
         return max(word_count.values())
 
     def calculate_frequency_for_word(self, text: str, word: str) -> int:
-        word_list = re.findall(
-            r'(?<![A-Za-z])[A-Za-z]+(?![A-Za-z])', text.lower())
+        '''
+        This method calculates the frequency of the given word in the given text
+
+        Args:
+            text (str): Text that needs to be analysed
+            word (str): Word whose frequency needs to be counted
+
+        Returns:
+            frequency_count (int): count of the given word's frequency
+        '''
+
+        word_list = self._constructWordList(text)
 
         if (not word_list):
             return 0
@@ -34,8 +55,18 @@ class WordFrequencyAnalyzer:
             return word_count[word.lower()]
 
     def calculate_most_frequent_n_words(self, text: str, number: str) -> List[WordFrequency]:
-        word_list = re.findall(
-            r'(?<![A-Za-z])[A-Za-z]+(?![A-Za-z])', text.lower())
+        '''
+        This method calculates the most frequent n number of words
+
+        Args:
+            text (str): Text that needs to be analysed
+            number (int): Number of frequenct words that needs to be returned
+
+        Returns:
+            most_frequent_n_words (List[WordFrequency]): List of most frequent words with length 'n'
+        '''
+
+        word_list = self._constructWordList(text)
 
         if (not word_list) or number == 0:
             return []
@@ -53,7 +84,37 @@ class WordFrequencyAnalyzer:
 
         return result
 
-    def _constructWordCountDict(self, word_list: list):
+    def _constructWordList(self, text: str) -> List[str]:
+        '''
+        This method splits the given text into words.
+
+        Defintion of word - a word is represented by a sequence of one or more
+        characters between 'a' and 'z' or between 'A and 'Z'). 
+        For example “agdfBh”. 
+
+        Args:
+            text (str): Text that needs to be processed
+
+        Returns:
+            list_of_words (List[str]): List of words that are split from the given text
+        '''
+
+        word_list = re.findall(
+            r'(?<![A-Za-z])[A-Za-z]+(?![A-Za-z])', text.lower())
+
+        return word_list
+
+    def _constructWordCountDict(self, word_list: list) -> Dict[str, int]:
+        '''
+        This method builds a dictionary of words and their respective count from the given list of words.
+
+        Args:
+            word_list (List[str]): List of words that needs to be converted to dict
+
+        Returns:
+            word_count_dict (Dict[str, int]): Dictionary of words with their respective count
+        '''
+
         word_count = {}
         for word in word_list:
             if word in word_count:
@@ -63,5 +124,5 @@ class WordFrequencyAnalyzer:
 
         return word_count
 
-    def _sort_dict_by_value_then_key(self, item: list):
+    def _sort_dict_by_value_then_key(self, item: list) -> Tuple:
         return (-item[1], item[0])
